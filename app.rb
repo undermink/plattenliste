35ff74c1fbad85365c9add29ea345363da44b8c9
undermink <undermink@devtal.de>
2013-03-07 00:58:04 +0100

    initial

---
+++ app.rb
@@ -0,0 +1,62 @@
+require 'sinatra/base'
+require 'data_mapper'
+require 'pp'
+
+DataMapper::Logger.new($stdout, :debug)
+DataMapper.setup(:default, (ENV["DATABASE_URL"]|| 'sqlite://'+File.expand_path('../platten.db',__FILE__)))
+DataMapper::Model.raise_on_save_failure=true
+
+class Music
+  include DataMapper::Resource
+  property :id, Serial
+  property :lfdnr, Text
+  property :interpret, Text
+  property :titel, Text
+  property :record, Text
+  property :size, Text
+  property :seite, Text
+  property :datum, Text
+  property :label, Text
+  property :no, Text
+  property :ablage, Text
+  property :dauer, Text
+  property :anzahl, Text
+  property :reihenfolge, Text
+  property :name, Text
+end
+
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
+
+
+class MusicApp < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+  
+  get '/neu' do
+    erb :neu
+  end
+  
+  post '/entry' do
+    Music.create!(params)
+      pp params
+    redirect '/'
+  end
+
+  get '/search' do
+    hash={}
+
+    Music.properties.each{|p|
+      hash[p.name.like]=params[p.name.to_s]+"%" if params[p.name.to_s]
+    }
+    #methods.sort
+#exit
+   # hash[:interprete.like]=params["interprete"]+"%" if params["interprete"]
+    @music=Music.all(hash)
+    erb :result
+  end
+
+end
---
+++ import.rb
@@ -0,0 +1,33 @@
+require './app.rb'
+require 'pp'
+
+File.open("Musikdatei_02.csv"){|f|
+  f.each_line{|line|
+    if line.length>2
+      cols=line[1..-2].split('","')
+      if cols.length>6
+	pp cols
+	u=Music.create({
+
+	  :lfdnr=>cols[0],
+	  :interpret=>cols[1],
+	  :titel=>cols[2],
+	  :record=>cols[3],
+	  :size=>cols[4],
+	  :seite=>cols[5],
+	  :datum=>cols[6],
+	  :label=>cols[7],
+	  :no=>cols[8],
+	  :ablage=>cols[9],
+	  :dauer=>cols[10]+" min",
+	  :anzahl=>cols[11],
+	  :reihenfolge=>cols[12],
+	  :name=>cols[13]})
+
+
+	  #      exit
+      end
+    end
+  }
+}
+
